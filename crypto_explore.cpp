5c82e98a2b336b039434a8dc174e6a4e68f86dd6
Jimmy Mendez <54858901+Jimmy-Mendez@users.noreply.github.com>
2021-11-10 16:32:09 -0500

    update classes

--- crypto_explore.cpp
+++ crypto_explore.cpp
@@ -4,9 +4,43 @@
 using namespace std;
 
 
-class transaction{
-	public:
+struct Transaction{
+		double amount;
+		string senderKey;
+		string recieveKey;
+		time_t timestamp;
+};
 
+class Block
+{
+private:
+	int index;
+	size_t blockHash;
+	size_t previousHash;
+	size_t generatehash();
+
+public:
+	//constructor
+	Block(int idx, TransactionData d, size_t prevHash);
+
+	size_t getHash();
+	size_t getPreviousHash();
+	TransactionData data;
+	bool isHashValid();
+};
+
+class Blockchain
+{
 	private:
+		Block createGenesisBlock();
+		
+	public:
+		vector<Block> chain;
 
+		Blockchain();
+
+		void addBlock(TransactionData data);
+		void isChainValid();
+
+		Block *getLatestBlock();
 };
